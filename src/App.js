7ed04c30da838efe5503bca44222d625984ee686
Samantha Oh <sami_oh@berkeley.edu>
2021-10-27 01:17:03 -0700

    deploy website 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 //import { Navigation, Footer, AboutMe, Work, Art, Contact, Xkeeper } from "./components";
 import { Scroll, AboutMe, Work, Art, Contact, Xkeeper, Omega, Buildabear, Findyourzen, Bloom, Nbjc } from "./components";
 function App() {
