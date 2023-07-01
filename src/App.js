628bd3d0f0b251605f6573c3d4d34dba59dacd8f
Samantha Oh <sami_oh@berkeley.edu>
2023-06-30 17:01:36 -0700

    redirect to new site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
 //import { Navigation, Footer, AboutMe, Work, Art, Contact, Xkeeper } from "./components";
-import { Scroll, AboutMe, Work, Art, Contact, Xkeeper, Omega, Buildabear, Findyourzen, Bloom, Nbjc, BSC, Cadence } from "./components";
+// import { Scroll, AboutMe, Work, Art, Contact, Xkeeper, Omega, Buildabear, Findyourzen, Bloom, Nbjc, BSC, Cadence } from "./components";
+import { Typography } from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <div className="App" style={{padding: "20% 20% 20% 20%", textAlign:"center", backgroundColor: "#F2ECE2", minHeight: "100vw"}}>
+      <Typography>This site has been rebuilt @ samanthaoh.com</Typography>
+      <a
+                href="https://9samioh.github.io/samioh/#/"
+                rel="noopener noreferrer"
+                style={{color: "#333"}}
+              >
+                <Typography variant="body2" sx={{color: "#333"}}>TAKE ME THERE!!! :)</Typography>
+              </a>
+      {/* <Router>
       <Scroll>
-        {/* <Navigation /> */}
         <Switch>
           <Route path="/" exact component={() => <Work />} />
           <Route path="/aboutme" exact component={() => <AboutMe />} />
@@ -23,9 +31,8 @@ function App() {
           <Route path="/cadence" exact component={() => <Cadence />} />
 
         </Switch>
-        {/* <Footer /> */}
         </Scroll>
-      </Router>
+      </Router> */}
     </div>
   );
 }
